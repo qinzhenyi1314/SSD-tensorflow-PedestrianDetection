d2e44b5345152b848e4f3beeae28cffcd98b6ad3
qinzhenyi1314 <18810584970@163.com>
2019-07-15 20:51:05 +0800

    更新训练参数以及pychram工程
    更新训练参数以及pychram工程

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # SSD-tensorflow-PedestrianDetection
-Use SSD Detection Pedestrian
+用SSD进行行人检测
 
+1.运行docker images
+docker pull qinzhenyi1314/tensorflow:1.13.1-gpu-py3-tb-jpd-vnc-pycharm-faster-rcnn
+
+2.下载预训练模型
 链接：https://pan.baidu.com/s/1bBYUYOQWrKPRBf_h9hM-6w 提取码：eadx
  
--- train.sh
+++ train.sh
@@ -17,3 +17,5 @@ python train_ssd_network.py \
     --optimizer=adam \
     --learning_rate=0.001 \
     --batch_size=32
+
+#--train_dir=./logs/train/ --dataset_dir=./tfrecords/train --dataset_name=pascalvoc_2007 --dataset_split_name=train --model_name=ssd_300_vgg --checkpoint_path=./checkpoints/vgg_16.ckpt --checkpoint_model_scope=vgg_16 --checkpoint_exclude_scopes=ssd_300_vgg/conv6,ssd_300_vgg/conv7,ssd_300_vgg/block8,ssd_300_vgg/block9,ssd_300_vgg/block10,ssd_300_vgg/block11,ssd_300_vgg/block4_box,ssd_300_vgg/block7_box,ssd_300_vgg/block8_box,ssd_300_vgg/block9_box,ssd_300_vgg/block10_box,ssd_300_vgg/block11_box --trainable_scopes=ssd_300_vgg/conv6,ssd_300_vgg/conv7,ssd_300_vgg/block8,ssd_300_vgg/block9,ssd_300_vgg/block10,ssd_300_vgg/block11,ssd_300_vgg/block4_box,ssd_300_vgg/block7_box,ssd_300_vgg/block8_box,ssd_300_vgg/block9_box,ssd_300_vgg/block10_box,ssd_300_vgg/block11_box --save_summaries_secs=50  --save_interval_secs=500  --weight_decay=0.0005  --optimizer=adam --learning_rate_decay_type=exponential --learning_rate=0.01 --learning_rate_decay_factor=0.5 --end_learning_rate=0.0001 --num_epochs_per_decay=4.0 --batch_size=32
